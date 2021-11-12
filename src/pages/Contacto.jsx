e016957687412c564928d845db72234e01605ac6
juan camilo <juanfable@gmial.com>
2021-11-11 21:23:08 -0500

    commit prueba para ver a cual repositorio esta conectado

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Contacto = () => {
     return (
         <div>
-            hola, desde contacto
+            hola, desde contacto, cambio de prueba
         </div>
     )
 }
